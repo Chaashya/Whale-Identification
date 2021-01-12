668f42451e46d37ce6149e756e9b6ad4e3ad298c
Chaashya <fernandochaashya@gmail.com>
2021-01-12 16:05:54 +1000

    import update

--- util.py
+++ util.py
@@ -8,8 +8,11 @@ import pickle
 import random
 import sys
 import time
+import os
 from os.path import isfile
+from PIL import Image as pil_image
 
+fig = plt.figure(figsize=(8, 8), dpi=100,facecolor='w', edgecolor='k')
 #src = 
 #video_inp = '../data/' + src + '.MP4'
 #img_out = '../data/img/' + src + '.jpg'
