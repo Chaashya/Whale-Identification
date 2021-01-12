1da54b121be0ad989bf54da4b13484ce552f6210
Chaashya <fernandochaashya@gmail.com>
2021-01-12 21:04:18 +1000

    Update util.py

--- util.py
+++ util.py
@@ -13,7 +13,12 @@ from os.path import isfile
 from PIL import Image as pil_image
 
 fig = plt.figure(figsize=(8, 8), dpi=100, facecolor='w', edgecolor='k')
-train_imgs = os.listdir("../input/humpback-whale-identification/train")
+train_imgs = os.listdir("C:/Users/chaas/Wildlife-Identification/whale-data/train")
+for idx, img in enumerate(np.random.choice(train_imgs, 12)):
+    ax = fig.add_subplot(4, 20//5, idx+1, xticks=[], yticks=[])
+    im = pil_image.open("C:/Users/chaas/Wildlife-Identification/whale-data/train" + img)
+    plt.imshow(im)
+    
 #src = 
 #video_inp = '../data/' + src + '.MP4'
 #img_out = '../data/img/' + src + '.jpg'
